da333d663d2c972e47c56af18d85626c292fd070
dudduss <duddus@berkeley.edu>
2016-02-21 07:36:59 -0800

    radar chart all setup to enter values in

--- NFLHackMain.py
+++ NFLHackMain.py
@@ -1,8 +1,8 @@
 import scipy
 import numpy as np
-# import matplotlib.cm as cm
-# import matplotlib.pyplot as plt
-# import pylab as pl
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+import pylab as pl
 # import sklearn
 import csv
 import json
@@ -38,7 +38,8 @@ leagueAverages = {}
 playermetricRatios = {}
 playerMetricScores = {}
 
-
+# plt.plot([1,2,3,4])
+# plt.show()
 
 """For team json file in TeamRoster, retrieves all ID's for provided position"""
 def getPlayers():
@@ -540,7 +541,7 @@ def setScoresForEachMetric():
 		index+=1
 
 
-	print playerMetricScores
+	# print playerMetricScores
 
 
 
@@ -591,6 +592,67 @@ setLeagueAverages()
 calcRBOutsideYardage()
 
 setScoresForEachMetric()
+
+
+
+""" Printing out the graph """
+
+
+class Radar(object):
+
+    def __init__(self, fig, titles, labels, rect=None):
+        if rect is None:
+            rect = [0.05, 0.05, 0.95, 0.95]
+
+        self.n = len(titles)
+        self.angles = np.arange(90, 90+360, 360.0/self.n)
+        self.axes = [fig.add_axes(rect, projection="polar", label="axes%d" % i) 
+                         for i in range(self.n)]
+
+        self.ax = self.axes[0]
+        self.ax.set_thetagrids(self.angles, labels=titles, fontsize=14)
+
+        for ax in self.axes[1:]:
+            ax.patch.set_visible(False)
+            ax.grid("off")
+            ax.xaxis.set_visible(False)
+
+        for ax, angle, label in zip(self.axes, self.angles, labels):
+            ax.set_rgrids(range(1, 6), angle=angle, labels=label)
+            ax.spines["polar"].set_visible(False)
+            ax.set_ylim(0, 5)
+
+    def plot(self, values, *args, **kw):
+        angle = np.deg2rad(np.r_[self.angles, self.angles[0]])
+        values = np.r_[values, values[0]]
+        self.ax.plot(angle, values, *args, **kw)
+
+
+# titles = list("ABCDE")
+
+# labels = [
+#     list("abcde"), list("12345"), list("uvwxy"), 
+#     ["one", "two", "three", "four", "five"],
+#     list("jklmn")
+# ]
+
+fig = pl.figure(figsize=(6, 6))
+
+titles = ["Outside Running", "Inside Running", "Downfield Running", "Short Yardage Running" , "Pass Catching"]
+labels = [
+    ['0','5','10','15','20'],
+    ['0','5','10','15','20'],
+    ['0','5','10','15','20'],
+    ['0','5','10','15','20'],
+    ['0','5','10','15','20']
+]
+
+radar = Radar(fig, titles, labels)
+radar.plot([1, 3, 2, 5, 4],  "-", lw=2, color="b", alpha=0.4, label="first")
+radar.plot([2.3, 2, 3, 3, 2],"-", lw=2, color="r", alpha=0.4, label="second")
+radar.plot([3, 4, 3, 4, 2], "-", lw=2, color="g", alpha=0.4, label="third")
+radar.ax.legend()
+plt.show()
 
 
 # print playermetricRatios
